a6081df50a124f39fd142f17371322559e985c97
Joe Fearnley <joe.fearnley@gmail.com>
2017-12-07 16:01:35 +0000

    Adding day 2 solution

--- src/day2.js
+++ src/day2.js
@@ -1,17 +1,13 @@
 exports.checksum = input => {
-  const rows = input.split("\n").map(number => {
-    return parseInt(number);
-  });
-  let checksum = 0;
-
-  const smallestLarget = rows.forEach(row => {
-    let smallest = Math.min(...rows[i]);
-    let largest = Math.max(...rows[i]);
-    return largest - smallest;
-  });
-
-  console.log(smallestLarget);
-
-  // for (let i = 0; i < rows.length; i++) {
-  // }
+  return input.split("\n").map(row => {
+    return row.split(" ").filter(number => {
+      return number !== "";
+    }).map(number => {
+      return parseInt(number);
+    });
+  }).reduce((accumulator, row) => {
+    let smallest = Math.min(...row);
+    let largest = Math.max(...row);
+    return accumulator + (largest - smallest);
+  }, 0);
 };
