31baa02e56e03f055b2ef46a69e693d2a9e1ccb7
Joe Fearnley <joe.fearnley@gmail.com>
2017-12-12 20:21:30 +0000

    Day 2 part 2

--- src/day2.js
+++ src/day2.js
@@ -11,3 +11,20 @@ exports.checksum = input => {
     return accumulator + (largest - smallest);
   }, 0);
 };
+
+exports.checksum2 = input => {
+  return input.split("\n").map(row => {
+      const numbers = row.split(" ");
+      let checksum = 0;
+      for (let i = 0; i < numbers.length; i++) {
+        for (let j = 0; j < numbers.length; j++) {
+          let result = parseInt(numbers[i]) / parseInt(numbers[j]);
+          if (Number.isInteger(result) && result !== 1) {
+            checksum += result;
+          }
+        }
+      }
+
+      return checksum;
+  }, 0).reduce((a, b) => a + b, 0);
+};
